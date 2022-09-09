5cdd367c855ab0db155a9cfc92d46bbd13aa755c
Макс Титов <hhit934@gmail.com>
2022-09-09 19:34:37 +0300

    first

--- js/main.js
+++ js/main.js
@@ -16,8 +16,19 @@ fetch('https://api.github.com/repos/Under4groos/SmdCompile.view/releases', {
     })
     .then(response => response.json())
     .then(data => {
-        if (!data || !data[0])
+        if (!data || !data[0]) {
             return
+        }
+
+        console.log(data[0]["created_at"])
+
+
+
+        let regExTime = /[0-9]+-?[0-9]+-[0-9]+/;
+        let time_ = regExTime.exec(data[0]["created_at"]);
+
+
+        last_ver_date.textContent = time_
         SetElement_onclick("last_ver", data[0]["html_url"]).textContent = data[0]["tag_name"]
         SetElement_onclick("but_view_demo", data[0]["assets"][0]["browser_download_url"])
         SetElement_onclick("but_buy", "https://vk.com/underko")
